119540525672a0fa70c1e15add7ee808939a5978
Al3x-T <alex.turianskyj@gmail.com>
2022-01-23 18:45:16 -0500

    commit

--- Back-end/themoviedb.js
+++ Back-end/themoviedb.js
@@ -11,8 +11,8 @@ var theMovieDb = {};
 
 theMovieDb.common = {
   api_key: "b232ad51ce7402046296b8bc8b7c35d8",
-  base_uri: "http://api.themoviedb.org/3/",
-  images_uri: "http://image.tmdb.org/t/p/",
+  base_uri: "https://api.themoviedb.org/3/",
+  images_uri: "https://image.tmdb.org/t/p/",
   timeout: 5000,
   language: "en-US",
   generateQuery: function(options) {
